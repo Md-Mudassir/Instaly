6260c44164b05fa4ad70a6df2cb12459eca78351
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-06 18:19:18 +0530

    added scripts

---
+++ accept.js
@@ -0,0 +1,28 @@
+getElementByXpath = path => {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+};
+setTimeout(() => {
+  getElementByXpath(
+    "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[2]/a"
+  ).click();
+  setTimeout(() => {
+    getElementByXpath(
+      "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[2]/div/div/div[4]/div/div/div/div/div"
+    ).click();
+    setTimeout(() => {
+      for (let i = 1; i < 100; i++) {
+        getElementByXpath(
+          "//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[2]/div/div/div[4]/div/div/div/div[" +
+            i +
+            "]/div[3]/div/div/button"
+        ).click();
+      }
+    }, 2000);
+  }, 3000);
+}, 3000);
